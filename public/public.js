6a8efa7996b5c1498b7b810965b6a36d32e08d1e
Imam ariadi <gamerapin139@gmail.com>
2024-08-10 11:04:32 +0700

    benerin server

--- public/public.js
+++ public/public.js
@@ -13,3 +13,20 @@ function toggleTeam() {
         toggleBtn.textContent = "Tampilkan Pengurus 2023";
     }
 }
+
+
+// navbar fixed
+// Ambil elemen navbar
+const navbar = document.querySelector('.navbar');
+
+// Fungsi untuk menambah atau menghapus kelas fixed pada navbar
+function fixNavbar() {
+    if (window.scrollY > navbar.offsetTop) {
+        navbar.classList.add('fixed');
+    } else {
+        navbar.classList.remove('fixed');
+    }
+}
+
+// Panggil fungsi fixNavbar setiap kali pengguna menggulir halaman
+window.addEventListener('scroll', fixNavbar);
