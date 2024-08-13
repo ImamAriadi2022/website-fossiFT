44eb28b28d001e155a1fb24c655ef977cd846398
Imam ariadi <gamerapin139@gmail.com>
2024-08-13 09:32:44 +0700

    fix toggle

--- public/public.js
+++ public/public.js
@@ -1,3 +1,22 @@
+function toggleTeam() {
+    var team2023 = document.getElementById("team-2023");
+    var team2024 = document.getElementById("team-2024");
+    var toggleBtn = document.getElementById("toggle-btn");
+
+    if (team2023.style.display === "none") {
+        team2023.style.display = "flex";
+        team2024.style.display = "none";
+        toggleBtn.textContent = "Tampilkan Pengurus 2024";
+    } else {
+        team2023.style.display = "none";
+        team2024.style.display = "flex";
+        toggleBtn.textContent = "Tampilkan Pengurus 2023";
+    }
+}
+
+
+
+
 // Navbar toggle untuk tablet dan handphone
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
